10e4774c8d45311e158189056393531cb97d8724
Jesse Weinstein <jesse@wefu.org>
2014-05-25 13:46:24 -0700

    Fix add_url

--- editing.py
+++ editing.py
@@ -166,11 +166,29 @@ class MusicBrainzClient(object):
             return self._check_response()
 
     def add_url(self, entity_type, entity_id, link_type_id, url, edit_note='', auto=False):
-        self.b.open(self.url("/edit/relationship/create_url", entity=entity_id, type=entity_type))
-        self._select_form("create_url")
-        self.b["ar.link_type_id"] = [str(link_type_id)]
-        self.b["ar.url"] = str(url)
-        return self._edit_note_and_auto_editor_and_submit_and_check_response('ar.',auto,edit_note,'already exists')
+        try:
+            self.b.open(self.url("/relationship-editor"), data=urllib.urlencode({
+                "rel-editor.rels.0.action":"add",
+                "rel-editor.rels.0.entity.0.gid":entity_id,
+                "rel-editor.rels.0.entity.0.type":entity_type,
+                "rel-editor.rels.0.entity.1.url":url,
+                "rel-editor.rels.0.entity.1.type":"url",
+                "rel-editor.rels.0.link_type":link_type_id,
+                "rel-editor.edit-note":edit_note,
+                "rel-editor.as_auto_editor":auto and 1 or 0}))
+        except urllib2.HTTPError, e:
+            if e.getcode()!=400:
+                raise Exception('unable to post edit', e)
+        try:
+            jmsg = json.load(self.b.response())
+        except ValueError, e:
+            raise Exception('unable to parse response as JSON', e)
+        if not jmsg.has_key('edits') or jmsg.has_key('error'):
+            raise Exception('unable to post edit', jmsg)
+        else:
+            if jmsg["edits"][0]["message"]=="no changes":
+                return False
+        return True
 
     def _update_entity_if_not_set(self, update, entity_dict, entity_type, item, suffix="_id", utf8ize=False, inarray=False):
         if item in update:
